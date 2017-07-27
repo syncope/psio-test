5311c1d8cb2ba171a7cce5ac542f9989375fe6fd
Christoph Rosemann <christoph.rosemann@desy.de>
2017-07-27 18:55:51 +0200

    mass commit: make this a pip-installable package with setup.py + MANIFEST.in to include all data files + added (!) the forgotten module by directory file __init__.py
    done?

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+with open(path.join('.', 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+from sphinx.setup_command import BuildDoc
+
+name='psio-test'
+version='0'
+release='0.1.0'
+
+setup(
+    name='psio-test',
+    version='0.1.0',
+
+    description='Module to test psio, an easy access library for photon science data in different formats.', 
+    long_description=long_description,
+
+    url='https://github.com/syncope/psio-test',
+
+    author='Ch.Rosemann',
+    author_email='christoph.rosemann@desy.de',
+    
+    license='GPLv2',
+    
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='photon science file input output test library',
+    
+    packages=['psio-test',],
+    
+    package_dir = { 'psio-test':'psio-test',},
+    
+)
+
